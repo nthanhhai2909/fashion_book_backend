575774b9f09b047570b56e1029569e443aa936d8
Nguyễn Thanh Hải <nthanhhai2909@gmail.com>
2018-05-01 14:00:04 +0700

    add bill model

---
+++ api/models/bill.model.js
@@ -0,0 +1,13 @@
+'use strict'
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const bill = new Schema ({
+    id_user: {
+        type: String,
+        required: [true, "can't be blank"],
+        index: true,
+    }
+})
+
+module.exports = mongoose.model('bill', bill);
