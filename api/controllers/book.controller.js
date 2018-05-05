a966f4a3f8c66dc94237ada6bff5783bf9628692
nhanng189 <nhanng189@gmail.com>
2018-05-05 15:07:19 +0700

    get-all-book

--- api/controllers/book.controller.js
+++ api/controllers/book.controller.js
@@ -2,7 +2,7 @@
 const book = require('../models/book.model');
 
 exports.getTotalPage = (req, res) => {
-    book.find ({},(err, docs) => {
+    book.find ({}, (err, docs) => {
         if(err){
             console.log(err);
             res.status(500).json({msg: err});
@@ -10,4 +10,42 @@ exports.getTotalPage = (req, res) => {
         }
         res.status(200).json({data: parseInt((docs.length - 1) / 9) + 1})
     })
+}
+
+exports.getAllBook = async (req,res) => {
+    if ((typeof req.body.page === 'undefined')) {
+        res.status(422).json({ msg: 'Invalid data' });
+        return;
+    }
+
+    let {page} = req.body;
+
+    let bookFind = null;
+    try {
+        bookFind = await book.find ({});
+    }
+    catch (err) {
+        res.status(500).json({ msg: err });
+        return;
+    }
+
+    let totalPage = await parseInt(((bookFind.length - 1) / 9) + 1);
+    
+    if ((page > totalPage) || (page < 1)) {
+        res.status(409).json({ msg: 'invalid page'});
+        return;
+    }
+
+    book
+    .find ({})
+    .skip( 9 * (page - 1) )
+    .limit(9)
+    .exec((err, docs) => {
+        if(err){
+            console.log(err);
+            res.status(500).json({msg: err});
+            return;
+        }
+        res.status(200).json({data: docs});
+    });
 }
