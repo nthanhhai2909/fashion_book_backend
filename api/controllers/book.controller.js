77e0b93857b277d343e799109a9abc2dcfcea992
nhanng189 <nhanng189@gmail.com>
2018-05-05 15:27:05 +0700

    update-get-all-book

--- api/controllers/book.controller.js
+++ api/controllers/book.controller.js
@@ -2,50 +2,50 @@
 const book = require('../models/book.model');
 
 exports.getTotalPage = (req, res) => {
-    book.find ({}, (err, docs) => {
-        if(err){
+    book.find({}, (err, docs) => {
+        if (err) {
             console.log(err);
-            res.status(500).json({msg: err});
+            res.status(500).json({ msg: err });
             return;
         }
-        res.status(200).json({data: parseInt((docs.length - 1) / 9) + 1})
+        res.status(200).json({ data: parseInt((docs.length - 1) / 9) + 1 })
     })
 }
 
-exports.getAllBook = async (req,res) => {
+exports.getAllBook = async (req, res) => {
     if ((typeof req.body.page === 'undefined')) {
         res.status(422).json({ msg: 'Invalid data' });
         return;
     }
 
-    let {page} = req.body;
+    let { page } = req.body;
 
-    let bookFind = null;
+    let bookCount = null;
     try {
-        bookFind = await book.find ({});
+        bookCount = await book.count({});
     }
     catch (err) {
         res.status(500).json({ msg: err });
         return;
     }
 
-    let totalPage = await parseInt(((bookFind.length - 1) / 9) + 1);
-    
+    let totalPage = await parseInt(((bookCount - 1) / 9) + 1);
+
     if ((page > totalPage) || (page < 1)) {
-        res.status(409).json({ msg: 'invalid page'});
+        res.status(409).json({ msg: 'Invalid page' });
         return;
     }
 
     book
-    .find ({})
-    .skip( 9 * (page - 1) )
-    .limit(9)
-    .exec((err, docs) => {
-        if(err){
-            console.log(err);
-            res.status(500).json({msg: err});
-            return;
-        }
-        res.status(200).json({data: docs});
-    });
+        .find({})
+        .skip(9 * (page - 1))
+        .limit(9)
+        .exec((err, docs) => {
+            if (err) {
+                console.log(err);
+                res.status(500).json({ msg: err });
+                return;
+            }
+            res.status(200).json({ data: docs, totalPage });
+        });
 }
