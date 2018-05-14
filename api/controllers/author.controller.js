8cbba7061f311ea249d78db93ab2c282da587969
Nguyễn Thanh Hải <nthanhhai2909@gmail.com>
2018-05-14 15:03:55 +0700

    add get name author by id

--- api/controllers/author.controller.js
+++ api/controllers/author.controller.js
@@ -22,3 +22,24 @@ exports.getAll = (req, res) => {
         res.status(200).json({data: docs})
     })
 }
+
+exports.getNameByID = async(req, res) => {
+    if(req.params.id === 'undefined') {
+        res.status(422).json({ msg: 'Invalid data' });
+        return;
+    }
+    let result
+    try {
+        result = await author.findById(req.params.id);
+    }
+    catch(err) {
+        console.log(err)
+        res.status(500).json({msg: err})
+        return;
+    }
+    if(result === null){
+        res.status(404).json({msg: "not found"})
+        return;
+    }
+    res.status(200).json({name: result.name})
+}
--- api/routers/author.router.js
+++ api/routers/author.router.js
@@ -3,4 +3,6 @@ const author_controller = require('../controllers/author.controller');
 module.exports = (app) => {
     app.route('/author/all')
         .get(author_controller.getAll);
+    app.route('/author/name/:id')
+        .get(author_controller.getNameByID)
 }
