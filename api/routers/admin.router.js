a5f869efeb140220080914cbbaeef5d7a4f99856
nguyen-pnt <pntrnguyen@gmail.com>
2018-05-23 18:42:09 +0700

    Update category

--- api/routers/admin.router.js
+++ api/routers/admin.router.js
@@ -11,4 +11,6 @@ module.exports = (app) => {
         .get(admin_controller.deleteUser);
     app.route('/admin/addcategory')
         .post(admin_controller.addCategory);
+    app.route('/admin/updatecategory')
+        .post(admin_controller.updateCategory);
 }
