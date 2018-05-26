c6036fbf78b5a90efa44b9d5ab17d7c5e14f090b
Nguyen Thanh Hai <nthanhhai2909@gmail.com>
2018-05-26 21:05:19 +0700

    apply all request

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
 //app.use(bodyParser.json());
 //app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+app.use(cors())
 
 userRouter(app);
 categoryRouter(app);
