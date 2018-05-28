30b1ee0a4e4491d87cf19bc672e7f10bbf84bb1c
Nguyen Thanh Hai <nthanhhai2909@gmail.com>
2018-05-29 06:35:26 +0700

    fix add to cart

--- api/controllers/cart.controller.js
+++ api/controllers/cart.controller.js
@@ -1,18 +1,19 @@
 'use strict'
 const cart = require('../models/cart.model');
 exports.addToCart = async (req, res) => {
-    if(typeof req.body.id === 'undefined'
+    if(typeof req.body.id_user === 'undefined'
     || typeof req.body.products === 'undefined') {
         res.status(422).json({msg: 'invalid data'});
         return;
     }
-    const { id, products } = req.body;
-    let cartFind
+    const { id_user, products } = req.body;
+    var cartFind
     try {
-        cartFind = await cart.findById(id)
+        cartFind = await cart.findOne({id_user: id_user})
     }
     catch(err) {
         const cart_new = new cart ({
+            id_user: id_user,
             products: products
         })
         let cartsave;
@@ -20,13 +21,14 @@ exports.addToCart = async (req, res) => {
             cartsave = await cart_new.save()
         }
         catch(err) {
-            res.status(500).json({msg: err, id_cart: cartsave._id})
+            res.status(500).json({msg: err})
             return;
         }
         return;
     }
     if(cartFind === null) {
         const cart_new = new cart ({
+            id_user: id_user,
             products: products
         })
         let cartsave;
@@ -34,22 +36,24 @@ exports.addToCart = async (req, res) => {
             cartsave = await cart_new.save()
         }
         catch(err) {
-            res.status(500).json({msg: err, id_cart: cartsave._id})
+            res.status(500).json({msg: err})
             return;
         }
+        return;
     }
-    let index = cartFind.products.findIndex(element => products[0]._id === element._id)
+    let index = cartFind.products.findIndex(element => products._id === element._id)
     if(index === -1) {
-        cartFind = [...cart,  ...products]
+        cartFind.products.push(products)
     } else {
-        cartFind.products[index].count = parseInt(cart[index].count) + parseInt(product.count)
+        cartFind.products[index].count = parseInt(cartFind.products[index].count) + parseInt(products.count);
+        console.log(cartFind.products[index].count)
     }
     try {
         cartFind.save()
     }
     catch(err) {
-        res.status(500).json({msg: err, id_cart: cartFind._id})
+        res.status(500).json({msg: err})
         return;
     }
-    res.status(200).json({msg: 'success', id_cart: cartFind._id});
+    res.status(200).json({msg: 'success'});
 }
--- api/models/cart.model.js
+++ api/models/cart.model.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const cart = new Schema ({
+    id_user: {
+        type: String,
+    },
     date: {
         type: Date,
         default: new Date()
